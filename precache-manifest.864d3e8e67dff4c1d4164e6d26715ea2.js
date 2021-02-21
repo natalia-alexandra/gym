1adbcb59d8d13fde97f8a756f458a71b24b30c8e
Natalia <natalia_alexandra@web.de>
2021-02-21 18:24:12 +0100

    Updates

---
+++ precache-manifest.864d3e8e67dff4c1d4164e6d26715ea2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc00d177006850e092a88fe5b33c010",
+    "url": "./index.html"
+  },
+  {
+    "revision": "33e1fb92dee9337fcff2",
+    "url": "./static/css/main.21feee60.chunk.css"
+  },
+  {
+    "revision": "4ffaf376c20c955f8394",
+    "url": "./static/js/2.52594902.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.52594902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33e1fb92dee9337fcff2",
+    "url": "./static/js/main.4869753c.chunk.js"
+  },
+  {
+    "revision": "2bc20bb9523d1033a237",
+    "url": "./static/js/runtime-main.99ffbf97.js"
+  },
+  {
+    "revision": "4290131e01bd54247563afe47da086a2",
+    "url": "./static/media/men-figur.4290131e.png"
+  }
+]);
